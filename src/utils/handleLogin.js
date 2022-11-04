8dc24c2f50ab67bee7e425ac053d3f498d553c47
Diogo Sales <diogosales.ads@gmail.com>
2022-11-04 20:54:27 -0300

    req4

--- src/utils/handleLogin.js
+++ src/utils/handleLogin.js
@@ -2,21 +2,25 @@ const crypto = require('crypto');
 
 const isValidEmail = (req, res, next) => {
   const { email } = req.body;
+  const emailValidation = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
-  if (
-    !email
-    || !email.includes('@')
-    || !email.includes('.com')
-  ) res.status(400).json({ message: 'invalid email' });
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!emailValidation.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
 
   next();
 };
 
 const isValidPassword = (req, res, next) => {
   const { password } = req.body;
-
-  if (!/^[0-9]{4,8}$/.test(password)) {
-    return res.status(400).json({ message: 'invalid password' });
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
 
   next();
